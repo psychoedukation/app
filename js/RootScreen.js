5abe92870efe28a41d3e7f9832a47c1c973c2f75
timoblume <info@timoblume.com>
2020-03-22 00:44:15 +0100

    Assets added, splash screen styles applied

--- js/RootScreen.js
+++ js/RootScreen.js
@@ -65,9 +65,21 @@ export default class RootScreen extends React.Component {
     }
     
     return (
-      <View style={{width: 250}}>
-        <Text>Hallo Test</Text>
-        <Text>{response}</Text>
+      <View style={{width: '100%', height: '100%', alignItems:'center'}}>
+        <View style={{width:'100%',height:'50%',backgroundColor: '#1DCCB1', justifyContent:'center', alignItems:'center'}}>
+          <Image
+            style={{width: 180, height: 180}}
+            source={require('../assets/img/avatar.png')}
+          />
+        </View>
+        <View style={{width:'100%', height:'20%'}}>
+        <Image
+            style={{width: '100%', overflow:'visible'}}
+            source={require('../assets/img/border-image.png')}
+          />
+        </View>
+
+        <Text style={{fontSize:36, color:'#45718D',paddingTop:20, fontWeight:'bold'}}>Corona Psycare</Text>
       </View>
     );
 
